addecb2662712f06d18545b81c0b704d9f7ee9cb
Kriptobro <79544857+Kriptobro@users.noreply.github.com>
2021-02-24 09:32:24 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 python bot.py +77002168301 doge
 python bot.py +77002168301 zec
 python bot.py +77002168301 ltc
-python bot.py +[phone1] btc
-python bot.py +[phone1] bch
+python bot.py +77002168301 btc
+python bot.py +77002168301 bch
 clear
 pwd
 bash start.sh
